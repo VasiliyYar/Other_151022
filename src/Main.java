90a9991451cb38a4d1797d34f1518b9a87d1dd3e
Vasiliy Solovev <vasya.79@bk.ru>
2022-10-18 22:29:15 +0300

    Combining

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static String[] products = { "Хлеб", "Пачка гречки", "Упаковка яиц", "Мороженка" };
-    public static int[] prices = { 50, 135, 65, 53 };
+    public static String[] products = {"Хлеб", "Пачка гречки", "Упаковка яиц", "Мороженка"};
+    public static int[] prices = {50, 135, 65, 53};
     public static int MIN_COST_FOR_BONUS = 1000;
 
     // В стоимости этих товаров каждые три товара должны стоить как два:
-    public static String[] productsOnSale = { "Хлеб", "Мороженка" };
+    public static String[] productsOnSale = {"Хлеб", "Мороженка"};
 
     public static void main(String[] args) {
         System.out.println("Добро пожаловать в магазин!");
@@ -38,14 +38,15 @@ public class Main {
 
         System.out.println("Ваша корзина покупок:");
         int sum = 0;
+        int sumBonus = 0;
         for (int i = 0; i < products.length; i++) {
-            sum += prices[i] * counts[i];
+            sumBonus += prices[i] * counts[i];
         }
-        boolean doBonus = sum >= MIN_COST_FOR_BONUS;
+        boolean doBonus = sumBonus >= MIN_COST_FOR_BONUS;
         for (int i = 0; i < products.length; i++) {
+
             if (counts[i] != 0) {
 
-                System.out.println("\t" + products[i] + " " + (doBonus ? counts[i] + 1 : counts[i]) + " шт. за " + (prices[i] * counts[i]) + " руб.");
 
                 boolean isOnSale = false;
                 for (String saleProduct : productsOnSale) {
@@ -57,7 +58,11 @@ public class Main {
                 if (isOnSale) {
                     System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + (prices[i] * (counts[i] / 3 * 2 + counts[i] % 3)) + " руб. (распродажа!)");
                     sum += prices[i] * (counts[i] / 3 * 2 + counts[i] % 3);
+                } else if (doBonus) {
+                    System.out.println("\t" + products[i] + " " + (doBonus ? counts[i] + 1 : counts[i]) + " шт. за " + (prices[i] * counts[i]) + " руб.");
+                    sum += prices[i] * counts[i];
                 } else {
+
                     System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + (prices[i] * counts[i]) + " руб.");
                     sum += prices[i] * counts[i];
                 }
